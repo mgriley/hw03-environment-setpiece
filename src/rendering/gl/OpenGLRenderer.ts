d589d250d58c90b1765363a49d6b290dbb45bd72
Matthew Riley <mgriley97@gmail.com>
2019-02-15 18:58:16 -0500

    started to setup the fbo for postprocessing

--- src/rendering/gl/OpenGLRenderer.ts
+++ src/rendering/gl/OpenGLRenderer.ts
@@ -6,7 +6,32 @@ import ShaderProgram from './ShaderProgram';
 
 // In this file, `gl` is accessible because it is imported above
 class OpenGLRenderer {
+  fbo: WebGLFramebuffer;
+  render_texture: WebGLTexture;
+  depth_buffer: WebGLRenderbuffer;
+
   constructor(public canvas: HTMLCanvasElement) {
+    // setup buffers for two-stage rendering
+    
+    // TODO - sizes?
+    this.render_texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, this.render_texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1024, 768, 0, gl.RGBA, gl.FLOAT, null);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+
+    this.depth_buffer = gl.createRenderbuffer()
+    gl.bindRenderbuffer(gl.RENDERBUFFER, this.depth_buffer);
+    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT, 1024, 768);
+
+    this.fbo = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.fbo);
+    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, this.depth_buffer);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.render_texture, 0);
+    gl.drawBuffers([gl.COLOR_ATTACHMENT0]);
+    if (gl.checkFramebufferStatus(gl.FRAMEBUFFER) != gl.FRAMEBUFFER_COMPLETE) {
+      console.error('framebuffer is  not complete')
+    }
   }
 
   setClearColor(r: number, g: number, b: number, a: number) {
@@ -23,6 +48,9 @@ class OpenGLRenderer {
   }
 
   render(camera: Camera, prog: ShaderProgram, drawables: Array<Drawable>, time: number) {
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
     prog.setEyeRefUp(camera.controls.eye, camera.controls.center, camera.controls.up);
     prog.setTime(time);
 
